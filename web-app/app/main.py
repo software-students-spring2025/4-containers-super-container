ad9b7d810ed5e43c84e10c1dcf1ec983642c7b52
Hao-Yang-Hao <hy2640@nyu.edu>
2025-04-06 02:26:45 -0400

    r

--- web-app/app/main.py
+++ web-app/app/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, send_from_directory
+"""Main application file for Flask server serving emotion data."""
+
+import os  # Standard library first
+from flask import Flask, render_template, send_from_directory  # Third-party
 from pymongo import MongoClient
-import os
 
 app = Flask(__name__)
 client = MongoClient("mongodb://mongo:27017/")  # 或 localhost，如在本地测试
@@ -10,12 +12,15 @@ collection = db["emotions"]
 
 @app.route('/')
 def index():
+    """Render the homepage with sorted emotion records."""
     records = list(collection.find().sort("timestamp", -1))
     return render_template("index.html", records=records)
 
 @app.route('/images/<path:filename>')
 def serve_image(filename):
+    """Serve images from the ML client image directory."""
     return send_from_directory("../machine-learning-client/images", filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    
